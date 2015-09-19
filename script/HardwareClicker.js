43c11aa68d7c182b0923e55a9fa5f80955087ac1
Frede175 <fsr1998@hotmail.com>
2015-09-19 16:12:37 +0200

    Clicking + some more prefix

--- script/HardwareClicker.js
+++ script/HardwareClicker.js
@@ -4,39 +4,51 @@ $(document).ready(function() {
 
 });
 
-var money = 0.00;
-var fps = 30;
-var perSec = 1000;
-var perTap = 0.02;
-var perLoop = 0;
-var numPrefix = -1;
-var divideby = 1;
-var i = 1;
+//Drawing
 
-var prefix = ["K", "M", "B", "T", "q", "Q", "s", "S", "O", "N", "d", "U", "D", "!", "@", "#", "$", "%", "^", "&", "*", "*1", "*2", "*3", "*4", "*5", "*6"];
+var fps = 30;
+
+//Stats
+
+var money = 0.00;
+var perSec = 100000000000000000000000000000000000000000;
+var perTap = 148635489463541321368849839848963510987984089709;
+var perLoop = 0;
+
+//Prefix
+
+var numPrefix = 0;
+var currentPrefix = [0,0,0];
+var prefix = ["", "K", "M", "B", "T", "q", "Q", "s", "S", "O", "N", "d", "U", "D", "!", "@", "#", "$", "%", "^", "&", "*", "*1", "*2", "*3", "*4", "*5", "*6", "*7", "*8", "*9", "*10"];
 
 
 
 function startGame() {
 	perLoop = calcPerLoop(perSec);
+	$('#perTap').text("$ " + parseFloat(calcPrefix(perTap, 2)).toFixed(3) + " " + prefix[currentPrefix[2]] + " per tap");
+	$('#perSec').text("$ " + parseFloat(calcPrefix(perSec, 1)).toFixed(3) + " " + prefix[currentPrefix[1]] + " per second");
+	Click();
 }
+
+/* ---------------- Click ---------------- */
+
+function Click() {
+	$('#Clicker').click(function(event) {
+		money += perTap;
+	});
+
+
+}
+
+
+
+
 
 function calcPerLoop(number) {
 	return number/fps;
 }
 
-/*while (true) {
-		if (number < 1000) {
-			return -1;
-		}
-		
-
-		i++;
-		x+=3;
-	}*/
-
-function getInfoString(number) {
-	var x = numPrefix+1;
+function getInfoString(number, x) {
 	var returnnum = number;
 	while (x > 0) {
 		returnnum /= 1000;
@@ -45,27 +57,31 @@ function getInfoString(number) {
 	return returnnum;
 }
 
-function calcPrefix() {
+function calcPrefix(number, n) {
 	
-	var l = parseFloat(getInfoString(money)).toFixed(0).toString().length;
-	
+	var l = parseFloat(getInfoString(number, x)).toFixed(0).toString().length;
+	var x = 0;
+
 	while (l >= 4) {
-		numPrefix++;
-		var l = parseFloat(getInfoString(money)).toFixed(0).toString().length;
+		x++;
+		l = parseFloat(getInfoString(number, x)).toFixed(0).toString().length;
 	}
+	currentPrefix[n] = x;
+	return getInfoString(number, x);
 }
 
 
-gameLoop=function() {
+/* ---------------- Main game loop ---------------- */
+
+gameLoop = function() {
 	money += perLoop;
-	calcPrefix();
-	if (numPrefix == -1 ) {
-		$('#money').text("$ " + (parseFloat(money).toFixed(3)));
-	}
-	else
-	{
-		$('#money').text("$ " + parseFloat(getInfoString(money)).toFixed(3) + " " + prefix[numPrefix]);
-	}
-
+	$('#money').text("$ " + parseFloat(calcPrefix(money, 0)).toFixed(3) + " " + prefix[currentPrefix[0]]);
 	setTimeout(gameLoop, 1000/fps);
-}
+};
+
+
+/* ---------------- Main logic ---------------- */
+
+gameLogic = function() {
+
+};
